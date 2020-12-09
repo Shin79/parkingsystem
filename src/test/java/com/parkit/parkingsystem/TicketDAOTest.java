abf401e9cb71a40edc4f703ee09c4b9be01d873a
Shin79 <sg.goto@gmail.com>
2020-12-09 17:37:01 +0100

    Création du fichier de test TicketDAOTest et ajout du test saveTicketTest()

---
+++ src/test/java/com/parkit/parkingsystem/TicketDAOTest.java
@@ -0,0 +1,38 @@
+package com.parkit.parkingsystem;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.dao.TicketDAO;
+import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
+
+public class TicketDAOTest{
+	private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
+	private static TicketDAO ticketDAO;
+	
+	@BeforeAll
+	public static void setUp() throws Exception{
+		ticketDAO = new TicketDAO();
+		ticketDAO.dataBaseConfig = dataBaseTestConfig;
+	}
+	@Test
+	public void saveTicketTest() {
+		Ticket ticket = new Ticket();
+		ParkingSpot parkingSpot = new ParkingSpot(1,ParkingType.CAR,false);
+		ticket.setParkingSpot(parkingSpot);
+		ticket.setVehicleRegNumber("ABCDEF");
+		ticket.setPrice(Fare.CAR_RATE_PER_HOUR);
+		LocalDateTime inTime = LocalDateTime.now();
+		ticket.setInTime(inTime);
+		boolean isFalse = ticketDAO.saveTicket(ticket);
+		assertEquals(isFalse,Boolean.FALSE);
+		
+	}
+}
