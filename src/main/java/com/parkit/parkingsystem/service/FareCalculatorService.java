ccd98b3d8681535df815b120bfc438760276bb3c
Shin <shin@gmail.com>
2020-12-10 19:07:28 +0100

    Modification de la condition d'obtention de la remise pour les clients récurrents avec un opérateur ternaire

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -8,34 +8,36 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket, boolean recurringUser){
-        if( (ticket.getOutTime() == null) || ticket.getOutTime().isBefore(ticket.getInTime()) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
-        }
+	public void calculateFare(Ticket ticket, boolean recurringUser) {
+		if ((ticket.getOutTime() == null) || ticket.getOutTime().isBefore(ticket.getInTime())) {
+			throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
+		}
 
-        LocalDateTime inHour = ticket.getInTime();
-        LocalDateTime outHour = ticket.getOutTime();
-                        
-        long diffInOut = ChronoUnit.MINUTES.between(inHour, outHour);  // Calcul de la différence entre heure d'entrée et de sortie
-        double duration = ((double)diffInOut / 60);   				  //Transformation en heure 
-        
-        if(duration <= Fare.FREE_HOURS) {                             // 30 premières minutes gratuites
-        	duration = Fare.FREE_PRICE;
-        } 
-        if(recurringUser) {                                               // Si utilisateur récurrent, calcul du  nouveau tarif
-        	duration *= Fare.DISCOUNT_RATE;
-        }
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-            	ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
-            }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
-        }
-    }
+		LocalDateTime inHour = ticket.getInTime();
+		LocalDateTime outHour = ticket.getOutTime();
+
+		long diffInOut = ChronoUnit.MINUTES.between(inHour, outHour); // Calcul de la différence entre heure d'entrée et
+																		// de sortie
+		double duration = ((double) diffInOut / 60); // Transformation en heure
+
+		if (duration <= Fare.FREE_HOURS) { // 30 premières minutes gratuites
+			ticket.setPrice(Fare.FREE_PRICE);
+		} else {
+			double factor = recurringUser ? Fare.DISCOUNT_RATE : 1.0;
+
+			switch (ticket.getParkingSpot().getParkingType()) {
+			case CAR: {
+				ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR * factor);
+				break;
+			}
+			case BIKE: {
+				ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR * factor);
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unkown Parking Type");
+			}
+		}
+	}
 
 }
